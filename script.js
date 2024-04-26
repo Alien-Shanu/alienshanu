bdbe65e31faf11beb818367085c907270ae7c3d4
ALIEN SHANU <165456967+Alien-Shanu@users.noreply.github.com>
2024-04-27 02:42:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const COMMANDS = {
   
   hackbar:'Supported commands: <span class="code">download ⬇️</span>, <span class="code">installation ✅</span>',
   download:"<a href='./HBM Hackbar 2020-v.07.1.xpi' class='success link'><img src='./hkbar.png'></a> <a href='./Alien 2020 Red Button Theme.rar' class='success link'><img src='./theme.png'></a>",
-  installation:"<video width='150' height='120' controls><source src='./HBM.mp4' type='video/mp4'></a>",
+  installation:"<video width='300' height='240' controls><source src='./HBM.mp4' type='video/mp4'></a>",
   
   
   cry:'<embed src="./scarylaugh.mp3"autostart style="visibility:hidden" type="audio/mpeg"></embed>',
