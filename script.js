1ac7c1dddfc262537df79a08a351d4be939ef743
ALIEN SHANU <165456967+Alien-Shanu@users.noreply.github.com>
2024-04-27 01:34:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ const COMMANDS = {
   installation:"<video width='320' height='240' controls><source src='./HBM.mp4' type='video/mp4'></a>",
   
   
-  cry:'<embed src="./scarylaugh.mp3"autostart style="visibility:hidden" type="audio/mpeg"></embed>',
-  fuck you :'<embed src="./scarylaugh.mp3"autostart style="visibility:hidden" type="audio/mpeg"></embed>',
+  cry :'<embed src="./scarylaugh.mp3"autostart style="visibility:hidden" type="audio/mpeg"></embed>,<img src="./showme.png">',
+  fuckyou :'<embed src="./fk.mp3"autostart style="visibility:hidden" type="audio/mpeg"></embed>',
   
   
   help:
