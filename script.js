0df1bb499edbd7e53ca402fa66868482d4ff73e0
ALIEN SHANU <165456967+Alien-Shanu@users.noreply.github.com>
2024-04-27 01:49:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,50 +4,33 @@ window.onload = () => {
  }
 const COMMANDS = {
   whoami:
-    'Black Alien',
+    'Black Alien 👽',
   8055:
     'Supported commands: <span class="code">tools</span>, <span class="code">files</span>, <span class="code">showme</span>',
   tools:'Supported commands: <span class="code">hackbar</span>, <span class="code">shell</span>, <span class="code">ransomeware</span><span class="code">rat</span>',
   files:'Supported commands: <span class="code">notes</span>, <span class="code">hacking video</span>, <span class="code">pdf course</span><span class="code">links</span>',
-  showme:'<img src="./showme.png">',
-
+  showme:'<img src="./img/showme.png">',
+  
   hackbar:'Supported commands: <span class="code">download</span>, <span class="code">installation</span>',
-   download:"<a href='./HBM Hackbar 2020-v.07.1.xpi' class='success link'><img src='./hkbar.png'></a> <a href='./Alien 2020 Red Button Theme.json' class='success link'><img src='./theme.png'></a>",
-  installation:"<video width='320' height='240' controls><source src='./HBM.mp4' type='video/mp4'></a>",
-
-
+  download:"<a href='./HBM Hackbar 2020-v.07.1.xpi' class='success link'><img src='./hkbar.png'></a> <a href='./Alien 2020 Red Button Theme.json' class='success link'><img src='./theme.png'></a>",
+  installation:"<video width='150' height='120' controls><source src='./HBM.mp4' type='video/mp4'></a>",
+  
+  
   cry:'<embed src="./scarylaugh.mp3"autostart style="visibility:hidden" type="audio/mpeg"></embed>',
-
-  hi: "Entha Monoose modeyaanoo",
-
+  fuck:'<img src="./img/fkst.webp" alt="WebP rules." width="320" height="240"><embed src="./fk.mp3"autostart style="visibility:hidden" type="audio/mpeg"></embed>',
+  
+  
   help:
     'Supported commands: <span class="code">about</span>, <span class="code">experience</span>, <span class="code">education</span>, <span class="code">skills</span>, <span class="code">acknowledges</span>, <span class="code">certificates</span>, <span class="code">contact</span>',
-  about: "Hello ðŸ‘‹<br>I'm Alien Shanu. Information Security enthusiast, I do bug bounty hunting, Capture The Flag as a hobby and passion",
+  about: "Hello 👋<br>I'm Alien Shanu 👽. Information Security enthusiast, I do bug bounty hunting, Capture The Flag as a hobby and passion",
   skills: '<span class="code">Skill:</span>Pentesting, Programming, Bughunting, Grapic Design, 2D Animation, ...<br>',
   education: "unknown",
-
-
-
-  certificates: 'Supported commands: <span class="code">CEH</span>, <span class="code">eWPTX</span>,<span class="code">CRTP</span>,<span class="code">APT</span>,<span class="code">NULLCON</span>',
-
-  ceh: "<a href='https://www.alienshanu.me/CEH.jpg' class='success link'>CEH</a>",
-
-  ewptx: "<a href='https://www.alienshanu.me/eWPTX.jpg' class='success link'>eWPTX</a>",
-
-  crtp: "<a href='https://www.alienshanu.me/CRTP.jpg' class='success link'>CRTP</a>",
-
-  apt: "<a href='https://www.alienshanu.me/APT.jpg' class='success link'>APT</a>",
-
-  nullcon: "<a href='https://www.alienshanu.me/NULCON.jpg' class='success link'>NULLCON</a>",
-
-
-
-
+  certificates: "<a href='https://www.alienshanu.me/certificates' class='success link'>certificates</a>",
   resume: "<a href='./resume.pdf' class='success link'>resume.pdf</a>",
-  experience: "Unlimited ExperienceðŸ˜¥",
-  acknowledges: "<a href='xxxx' class='success link'>Google Hall of Fame</a> , <a href='xxxx' class='success link'>Nokia HOF</a> , <a href='xxxx' class='success link'>dev.to HOF</a> , <a href='xxxx' class='success link'>Netflix HOF</a> , <a href='xxxx' class='success link'>Pinterest HOF</a> , <a href='xxxx' class='success link'>Philips HOF</a> , <a href='xxxx' class='success link'>Soundcloud HOF</a> , <a class='success link'>many more...</a>  ",
-  contact: "You can contact me on any of following :<br><a href='https://t.me/Alien_Shanu/' class='success link'>Telegram</a>, <a href='https://www.instagram.com/alien_shanu/' class='success link'>Instagram</a>, <a href='https://www.twitter.com/Alien_Shanu/' class='success link'>Twitter</a>, <a href='mailto:mail@blackalien@pm.me' class='success link'>Email</a>, <a href='https://api.whatsapp.com/send?phone=+966547818575' class='success link'>whatsapp</a>",
-
+  experience: "No Experience😥",
+  acknowledges: "<a href='xxxx' class='success link'>Google Hall of Fame</a> , <a href='xxxx' class='success link'>Nokia HOF</a> , <a href='xxxx' class='success link'>dev.to HOF</a> , <a href='xxxx' class='success link'>Netflix HOF</a> , <a href='xxxx' class='success link'>Pinterest HOF</a> , <a href='xxxx' class='success link'>Philips HOF</a> , <a href='xxxx' class='success link'>Soundcloud HOF</a> , <a class='success link'>many more...</a>  ", 
+  contact: "You can contact me on any of following :<br><a href='https://t.me/Alien_Shanu/' class='success link'><i class='fab fa-telegram icon'></i>Telegram</a>,<a href='https://www.instagram.com/alien_shanu/' class='success link'><i class='fab fa-instagram icon'></i>Instagram</a>,  <a href='https://www.twitter.com/Alien_Shanu/' class='success link'><i class='fab fa-twitter icon'></i>Twitter</a>, <a href='mailto:mail@blackalien@pm.me' class='success link'><i class='far fa-envelope-open icon'></i>Email</a>, <a href='https://api.whatsapp.com/send?phone=+966547818575' class='success link'><i class='fab fa-whatsapp icon'></i>whatsapp</a>",
+ 
 };
 
 const  userInput = document.getElementById("userInput");
@@ -62,7 +45,7 @@ inputfield.addEventListener('keypress', (e) =>{
       return;
     }
     let output;
-    output = `<div class="terminal-line"><span class="code1">âžœ</span> <span class="success">Alien</span><span class="directory">@Shanu:~$</span> ${input}</div>`;
+    output = `<div class="terminal-line"><span class="code1">➜</span> <span class="success">Alien</span><span class="directory">@Shanu:~$</span> ${input}</div>`;
     if (!COMMANDS.hasOwnProperty(input)) {
       output += `<div class="terminal-line">no such command: ${input}</div>`;
       console.log("Oops! no such command");
@@ -72,12 +55,10 @@ inputfield.addEventListener('keypress', (e) =>{
     terminalOutput.innerHTML = `${
       terminalOutput.innerHTML
     }<div class="terminal-line">${output}</div>`;
-    terminalOutput.scrollTop = terminalOutput.scrollHeight;
+    terminalOutput.scrollTop = terminalOutput.scrollHeight; 
     e.target.value = ""
-
+ 
   };
 });
-
-
 
 
