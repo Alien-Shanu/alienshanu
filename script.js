62180f75b9a0c88ae4a406f086e03984751c8d8f
ALIEN SHANU <165456967+Alien-Shanu@users.noreply.github.com>
2024-11-14 06:44:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,14 +17,16 @@ const COMMANDS = {
   installation:"<video width='300' height='240' controls><source src='./HBM.mp4' type='video/mp4'></a>",
   
   rainmeter: 'Supported commands: <span class="code">alien_skin ⬇️</span>, <span class="code">skin_install ✅</span>',
-  alien_skin:"<a href='./Alien Rain Meter Skin_5.0.zip' class='success link'><img src='./rmtr.png'></a> <a href='./Alien 2020 Red Button Theme.rar' class='success link'><img src='./guide.png'></a>",
-  skin_install:"<video width='300' height='240' controls><source src='./HBM.mp4' type='video/mp4'></a>",
+  alien_skin:"<a href='./Alien Rain Meter Skin_5.0.zip' class='success link'><img src='./rmtr.png'></a> ",
+  skin_install:"<video width='300' height='240' controls><source src='./Rain.mp4' type='video/mp4'></a>",
   
   cry:'<img src="./fkst.webp" alt="WebP rules." width="320" height="240"><embed src="./scarylaugh.mp3"autostart style="visibility:hidden" type="audio/mpeg"></embed>',
   fuck:'<img src="./fkst.webp" alt="WebP rules." width="320" height="240"><embed src="./fk.mp3"autostart style="visibility:hidden" type="audio/mpeg"></embed>',
   hi: "Entha Monoose modeyaanoo",
   whoareyou: "Ninte kunjammede mon",
   hello: "Amme dhe oru pichakkaran",
+  iloveyou: "Ok, Can i kissyou ?",
+  willyoumarryme: "Why not , am ready to f**k"
 
 
   
