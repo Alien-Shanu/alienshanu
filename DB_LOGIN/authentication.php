8a91d31717ac59ed07ce9c4cf582556eb3f612ac
ALIEN SHANU <165456967+Alien-Shanu@users.noreply.github.com>
2024-12-27 03:49:01 +0300

    Update authentication.php

--- DB_LOGIN/authentication.php
+++ DB_LOGIN/authentication.php
@@ -2,7 +2,7 @@
 $host = "localhost";  
     $user = "root";  
     $password = '';  
-    $db_name = "db_login";       
+    $db_name = "dblogin.html";       
     $con = mysqli_connect($host, $user, $password, $db_name);  
   
 
